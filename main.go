c095df797dfb30efe717a03bdd41e89c59dd93c5
lkzx <lukas.koenn98@googlemail.com>
2026-03-05 19:55:30 +0100

    solve lesson 1-5

--- main.go
+++ main.go
@@ -6,13 +6,20 @@ import (
 	"os"
 )
 
+
 func main() {
+	cliCommands := getCliCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		inputString := scanner.Text()
-		inputList := cleanInput(inputString)
-		fmt.Printf("Your command was: %v\n", inputList[0])
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+		commandString := cleanedInput[0]
+		if command, ok := cliCommands[commandString]; ok {
+			command.callback()
+		} else {
+			fmt.Printf("unknown command: %s\n", commandString)
+		}
 	}
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,31 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCliCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
 
 func cleanInput(text string) []string {
 	res := []string{}
@@ -9,4 +34,18 @@ func cleanInput(text string) []string {
 		res = append(res, strings.ToLower(strings.TrimSpace(word)))
 	}
 	return res
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, command := range getCliCommands() {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
 }
